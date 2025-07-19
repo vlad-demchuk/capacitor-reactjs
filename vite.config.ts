ac7545bd2fd9380c39238cd85575e9fc70cb0f28
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2025-07-19 20:17:41 +0300

    Update configuration files and dependencies for improved linting and project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'node:path' // https://vitejs.dev/config/
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,10 +12,10 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  // },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
